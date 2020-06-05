f47d2aa4e8df9e40ab77fbd8951dedbb7fa1007c
mirodil1999 <mirodilkamilov1999@gmail.com>
2020-06-05 15:02:13 +0500

    some change

---
+++ resources/views/layouts/mainLayout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>OnlineShop</title>
+</head>
+<body>
+
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+        Bootstrap
+    </a>
+</nav>
+    @section('content')
+</body>
+</html>
