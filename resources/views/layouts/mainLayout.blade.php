bcec250b210537363dfa3a759df9a0f8d9a51d00
mirodil1999 <mirodilkamilov1999@gmail.com>
2020-06-17 17:20:42 +0500

    services part without photos (mobile version only)

--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -5,7 +5,9 @@
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="stylesheet" href="css/layout.css">
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
--- resources/views/primary/home.blade.php
+++ resources/views/primary/home.blade.php
@@ -36,30 +36,40 @@
             <span class="sr-only">Next</span>
         </a>
     </div>
-    <div class="content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid cupiditate distinctio ducimus error laboriosam
-            magni maxime nobis odio perferendis possimus, quaerat qui sequi, ullam! A animi asperiores, aut consectetur
-            cupiditate deserunt distinctio eaque enim eum eveniet explicabo fugiat illum in inventore ipsam labore maiores
-            nemo porro possimus quia reiciendis ullam vel! Consequatur cupiditate doloribus, ea error expedita harum hic
-            illum molestias neque nesciunt placeat quam quos reiciendis reprehenderit sapiente sint tempora tempore
-            temporibus, velit veritatis. A adipisci amet at atque, blanditiis corporis culpa debitis dolore dolores
-            doloribus esse exercitationem facere fugit illo in ipsam iusto laborum, magnam minus molestiae mollitia nam
-            natus omnis perspiciatis placeat provident quae quas quis quod quos, rem repellat repellendus sint temporibus
-            ullam voluptate voluptatem. Aperiam architecto consequatur deleniti deserunt ducimus eaque error impedit
-            laudantium libero maiores molestiae mollitia nihil numquam obcaecati odit porro quam ratione repellat sint,
-            tempora veritatis voluptatem voluptatum. Magnam nam provident recusandae saepe sint. Accusamus amet aperiam at
-            consequuntur cupiditate doloribus eius eligendi esse, expedita illo illum ipsa itaque modi nobis non nulla
-            obcaecati officia officiis optio perferendis perspiciatis quia quisquam sit soluta sunt tenetur ut? Doloribus
-            ducimus fugiat quam quod, similique tempora veniam voluptatum. Eos facilis maiores optio! Fuga, minus,
-            voluptas.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem doloribus enim excepturi ipsum magni repellat
-            tempora. Ab autem cum cumque delectus ducimus eligendi exercitationem facere id impedit ipsum iste iure odit
-            provident quia quos reprehenderit, saepe sapiente sit tempora veniam. Adipisci amet ea incidunt labore nesciunt
-            quaerat quasi quo saepe. Consequuntur corporis, debitis delectus eos odit reiciendis voluptate? Commodi,
-            corporis dolores eligendi est eum laborum neque rerum soluta? Consequatur consequuntur delectus dicta labore
-            minima provident quis soluta tempora vero voluptatum. A autem doloremque dolorum nam nesciunt reiciendis
-            repellendus sint! Culpa, deserunt dolorem doloribus illum magnam provident quisquam reiciendis vitae
-            voluptate.</p>
+
+    <div class="services">
+        <h1>Our services</h1>
+    </div>
+    <div class="card-deck">
+        <div class="card text-center">
+            <div class="card-header">
+                Fast and safe delivery
+            </div>
+            <div class="card-body">
+                <img src="css/delivery.png" alt="delivery">
+                <p class="card-text">We deliver to anywhere in the territory of Uzbekistan within <strong>3 working day</strong></p>
+            </div>
+        </div>
+
+        <div class="card text-center">
+            <div class="card-header">
+                Guarantee
+            </div>
+            <div class="card-body">
+                <img src="css/guarantee.png" alt="guarantee">
+                <p class="card-text">We guarantee all our products for <strong>1 year</strong></p>
+            </div>
+        </div>
+
+        <div class="card text-center">
+            <div class="card-header">
+                Pay any way
+            </div>
+            <div class="card-body">
+                <img src="css/pay.jpg" alt="pay">
+                <p class="card-text">You can pay by card online or by cash after receiving product</p>
+            </div>
+        </div>
     </div>
 
 
